afa974294fc4544f7bc0fffd8f8da538ed33da8d
wangwenjie <wangwenjie@dabuxiangqian.cn>
2017-10-18 12:55:33 +0800

    create program

---
+++ JWAlbum/JWAlbum/JWAblumView.swift
@@ -0,0 +1,21 @@
+//
+//  JWAblumView.swift
+//  JWAlbum
+//
+//  Created by JayWong on 2017/10/18.
+//  Copyright © 2017年 WWJ. All rights reserved.
+//
+
+import UIKit
+
+class JWAblumView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
